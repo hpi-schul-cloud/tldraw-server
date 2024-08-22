b2d3d60b8061162825b991044d302db7526750e1
SevenWaysDP <sergej.hoffmann@dataport.de>
2024-08-22 14:03:06 +0200

    refactor: Remove AppController

--- src/modules/server/api/app.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller()
-export class AppController {
-	@Get('/')
-	healthy(): string {
-		return 'hello tldraw';
-	}
-}
--- src/modules/server/server.module.ts
+++ src/modules/server/server.module.ts
@@ -5,12 +5,10 @@ import { AuthorizationModule } from '../../infra/authorization/authorization.mod
 import { LoggerModule } from '../../infra/logging/logger.module.js';
 import { RedisModule } from '../../infra/redis/index.js';
 import { StorageModule } from '../../infra/storage/storage.module.js';
-import { AppController } from './api/app.controller.js';
 import { UWS, WebsocketGateway } from './api/websocket.gateway.js';
 
 @Module({
 	imports: [ConfigModule.forRoot({ isGlobal: true }), RedisModule, StorageModule, AuthorizationModule, LoggerModule],
-	controllers: [AppController],
 	providers: [
 		WebsocketGateway,
 		{
