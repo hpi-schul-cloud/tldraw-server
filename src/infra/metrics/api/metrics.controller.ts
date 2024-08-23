9d8e4196201b1e1008fcbbb5c535062b865f7264
SevenWaysDP <sergej.hoffmann@dataport.de>
2024-08-23 12:35:42 +0200

    chore: Add Content-Type header to metrics endpoint

--- src/infra/metrics/api/metrics.controller.ts
+++ src/infra/metrics/api/metrics.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Header } from '@nestjs/common';
 import { MetricsService } from '../metrics.service.js';
 
 @Controller('/metrics')
 export class MetricsController {
 	constructor(private readonly appService: MetricsService) {}
 
+	@Header('Content-Type', 'text/plain')
 	@Get()
 	getMetrics(): Promise<string> {
 		return this.appService.getMetrics();
