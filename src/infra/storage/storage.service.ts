2a61d5537117a2b92169e9643e36efab36884bdd
Max Bischof <maximilian.bischof@dataport.de>
2024-09-16 15:24:35 +0200

    Remove hardcoded bucketname

--- src/infra/storage/storage.service.ts
+++ src/infra/storage/storage.service.ts
@@ -5,29 +5,30 @@ import { Logger } from '../logging/logger.js';
 @Injectable()
 export class StorageService {
 	private internalStorageInstance?: any;
+	private s3Endpoint: string;
+	private bucketName: string;
 
 	constructor(
 		private configService: ConfigService,
 		private logger: Logger,
 	) {
 		this.logger.setContext(StorageService.name);
+		this.s3Endpoint = this.configService.getOrThrow<string>('S3_ENDPOINT');
+		this.bucketName = this.configService.get<string>('S3_BUCKET') || 'ydocs';
 	}
 
 	public async get() {
-		const s3Endpoint = this.configService.get<string>('S3_ENDPOINT');
-		const bucketName = this.configService.get<string>('S3_BUCKET') || 'ydocs';
-
 		let store;
 
-		if (s3Endpoint) {
+		if (this.s3Endpoint) {
 			this.logger.log('using s3 store');
 			// @ts-expect-error - @y/redis is only having jsdoc types
 			const { createS3Storage } = await import('@y/redis/storage/s3');
 
-			store = createS3Storage(bucketName);
+			store = createS3Storage(this.bucketName);
 			try {
 				// make sure the bucket exists
-				await store.client.makeBucket(bucketName);
+				await store.client.makeBucket(this.bucketName);
 			} catch (e) {}
 		} else {
 			this.logger.log('ATTENTION! using in-memory store');
@@ -43,13 +44,13 @@ export class StorageService {
 		const store = await this.getInternalStorageInstance();
 
 		const objectsList = [];
-		const stream = store.client.listObjectsV2('ydocs', parentId, true);
+		const stream = store.client.listObjectsV2(this.bucketName, parentId, true);
 
 		for await (const obj of stream) {
 			objectsList.push(obj.name);
 		}
 
-		await store.client.removeObjects('ydocs', objectsList);
+		await store.client.removeObjects(this.bucketName, objectsList);
 	}
 
 	private async getInternalStorageInstance(): Promise<any> {
