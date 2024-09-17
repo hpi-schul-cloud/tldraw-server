e2cff88a09dc9983ed6dd6d3244138b59dc7b7fa
SevenWaysDP <sergej.hoffmann@dataport.de>
2024-09-17 07:57:59 +0200

    BC-7808 - Adjust eslint config in tldraw-server

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,25 +1,62 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: 'tsconfig.json',
+		tsconfigRootDir: __dirname,
+		sourceType: 'module',
+	},
+	plugins: ['@typescript-eslint/eslint-plugin', 'jest'],
+	// https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/src/configs/stylistic-type-checked.ts
+	extends: ['plugin:@typescript-eslint/stylistic-type-checked', 'plugin:prettier/recommended'],
+	root: true,
+	env: {
+		node: true,
+		jest: true,
+	},
+	ignorePatterns: ['.eslintrc.cjs'],
+	rules: {
+    "require-await": "error",
+    "no-return-assign": "error",
+    "max-classes-per-file": 'error',
+    "@typescript-eslint/explicit-member-accessibility": "error",
+    "@typescript-eslint/explicit-function-return-type": "error",
+		'@typescript-eslint/explicit-module-boundary-types': 'error',
+		'@typescript-eslint/no-explicit-any': 'error',
+		'@typescript-eslint/unbound-method': 'error',
+		'@typescript-eslint/no-unused-vars': 'error',
+    "@typescript-eslint/no-non-null-assertion": "error",
+    '@typescript-eslint/naming-convention': [
+			'error',
+			{
+				selector: 'interface',
+				format: ['PascalCase'],
+				custom: {
+					regex: '^I[A-Z]',
+					match: false,
+				},
+			},
+		],
+		'@typescript-eslint/no-empty-interface': [
+			'error',
+			{
+				allowSingleExtends: true,
+			},
+		],
+	},
+  overrides: [
+    {
+      files: ['**/*spec.ts'],
+      plugins: ['jest'],
+      env: {
+        jest: true,
+      },
+      rules: {
+        // you should turn the original rule off *only* for test files
+        '@typescript-eslint/unbound-method': 'off',
+        "@typescript-eslint/explicit-function-return-type": "off",
+        'jest/unbound-method': 'error',
+        'jest/prefer-spy-on': 'error',
+      },
+    },
   ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.cjs'],
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
 };
