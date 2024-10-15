4063ab119f4c1ee10f8bebd3fa5b2825e182241c
Constantin Bergatt <constantin.bergatt@dataport.de>
2024-10-15 10:42:29 +0200

    BC-7879 - add `ConfigService` to `AuthorizationModule` and exclude `authorization-api-client` from code coverage

--- src/infra/authorization/authorization.module.ts
+++ src/infra/authorization/authorization.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { LoggerModule } from '../logging/logger.module.js';
-import { AuthorizationApi } from './authorization-api-client/index.js';
+import { AuthorizationApi, Configuration } from './authorization-api-client/index.js';
 import { AuthorizationService } from './authorization.service.js';
 
 @Module({
 	imports: [LoggerModule],
-	providers: [AuthorizationApi, AuthorizationService],
+	providers: [
+		{
+			provide: AuthorizationApi,
+			useFactory: (configService: ConfigService): AuthorizationApi => {
+				const apiHost = configService.getOrThrow<string>('API_HOST');
+				const configuration = new Configuration({ basePath: apiHost });
+				const authorizationApi = new AuthorizationApi(configuration);
+
+				return authorizationApi;
+			},
+			inject: [ConfigService],
+		},
+		AuthorizationService,
+	],
 	exports: [AuthorizationService],
 })
 export class AuthorizationModule {}
