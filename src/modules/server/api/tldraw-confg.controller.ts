d07b1dbf7700091c4be822aeb7fc587fa73296aa
SevenWaysDP <sergej.hoffmann@dataport.de>
2024-10-16 15:57:30 +0200

    Refactor TldrawConfigController to use ServerConfig

--- src/modules/server/api/tldraw-confg.controller.ts
+++ src/modules/server/api/tldraw-confg.controller.ts
@@ -1,28 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ServerConfig } from '../server.config.js';
 import { TldrawPublicConfigResponse } from './dto/tldraw-config.response.js';
 
 @ApiTags('tldraw/config')
 @Controller('tldraw/config')
 export class TldrawConfigController {
-	public constructor(private readonly configService: ConfigService) {}
+	public constructor(private readonly config: ServerConfig) {}
 
 	@ApiOperation({ summary: 'Useable configuration for clients' })
 	@ApiResponse({ status: 200, type: TldrawPublicConfigResponse })
 	@Get('/public')
 	public publicConfig(): TldrawPublicConfigResponse {
-		const mappedConfig = {
-			TLDRAW__WEBSOCKET_URL: this.configService.getOrThrow('TLDRAW__WEBSOCKET_URL'),
-			TLDRAW__ASSETS_ENABLED: this.configService.getOrThrow('TLDRAW__ASSETS_ENABLED') === 'true',
-			TLDRAW__ASSETS_MAX_SIZE_BYTES: parseInt(this.configService.getOrThrow('TLDRAW__ASSETS_MAX_SIZE_BYTES')),
-			TLDRAW__ASSETS_ALLOWED_MIME_TYPES_LIST: this.configService
-				.getOrThrow('TLDRAW__ASSETS_ALLOWED_MIME_TYPES_LIST')
-				.split(','),
-			FEATURE_TLDRAW_ENABLED: this.configService.getOrThrow('FEATURE_TLDRAW_ENABLED') === 'true',
-		};
-
-		const response = new TldrawPublicConfigResponse(mappedConfig);
+		const response = new TldrawPublicConfigResponse(this.config);
 
 		return response;
 	}
