598112b74b771d80e2c16c4e66e501059584d7d2
Constantin Bergatt <constantin.bergatt@dataport.de>
2024-10-30 10:07:07 +0100

    BC-8093 - remove unnecessary `logger.types.ts`

--- src/infra/logger/index.ts
+++ src/infra/logger/index.ts
@@ -2,4 +2,3 @@ export * from './interfaces/index.js';
 export { LoggerConfig } from './logger.config.js';
 export * from './logger.js';
 export * from './logger.module.js';
-export * from './types/index.js';
