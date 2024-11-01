6e7803708883b1fa29b986abe53d7e387e37ee71
Max Bischof <maximilian.bischof@dataport.de>
2024-11-01 11:44:58 +0100

    Add missing tests of storage service

--- src/infra/storage/storage.service.spec.ts
+++ src/infra/storage/storage.service.spec.ts
@@ -271,4 +271,163 @@ describe('StorageService', () => {
 			});
 		});
 	});
+
+	describe('retrieveStateVector', () => {
+		describe('when retrieveDoc rejects', () => {
+			const setup = () => {
+				const error = new Error();
+				jest.spyOn(service, 'retrieveDoc').mockRejectedValueOnce(error);
+
+				return { error };
+			};
+
+			it('should throw an error', async () => {
+				const { error } = setup();
+
+				await expect(service.retrieveStateVector('room', 'docname')).rejects.toThrow(error);
+			});
+		});
+
+		describe('when retrieveDoc resolves', () => {
+			describe('when retrieveDoc returns null', () => {
+				const setup = () => {
+					jest.spyOn(service, 'retrieveDoc').mockResolvedValueOnce(null);
+
+					return null;
+				};
+
+				it('should return null', async () => {
+					setup();
+
+					const result = await service.retrieveStateVector('room', 'docname');
+
+					expect(result).toBeNull();
+				});
+			});
+
+			describe('when retrieveDoc returns doc', () => {
+				const setup = () => {
+					const doc = new Uint8Array(0);
+					jest.spyOn(service, 'retrieveDoc').mockResolvedValueOnce({ doc, references: [] });
+
+					const stateVector = new Uint8Array(1);
+					jest.spyOn(Y, 'encodeStateVectorFromUpdateV2').mockReturnValueOnce(stateVector);
+
+					return { doc, stateVector };
+				};
+
+				it('should call encodeStateVectorFromUpdateV2', async () => {
+					const { doc } = setup();
+
+					await service.retrieveStateVector('room', 'docname');
+
+					expect(Y.encodeStateVectorFromUpdateV2).toHaveBeenCalledWith(doc);
+				});
+
+				it('should return state vector', async () => {
+					const { stateVector } = setup();
+
+					const result = await service.retrieveStateVector('room', 'docname');
+
+					expect(result).toBe(stateVector);
+				});
+			});
+		});
+	});
+
+	describe('deleteReferences', () => {
+		describe('when removeObjects resolves', () => {
+			const setup = () => {
+				const storeReferences = ['ref1', 'ref2'];
+
+				return { storeReferences };
+			};
+
+			it('should call removeObjects', async () => {
+				const { storeReferences } = setup();
+
+				await service.deleteReferences('room', 'docname', storeReferences);
+
+				expect(client.removeObjects).toHaveBeenCalledWith(bucketName, storeReferences);
+			});
+		});
+
+		describe('when removeObjects rejects', () => {
+			const setup = () => {
+				const error = new Error();
+				client.removeObjects.mockRejectedValueOnce(error);
+
+				return { error };
+			};
+
+			it('should throw an error', async () => {
+				const { error } = setup();
+
+				await expect(service.deleteReferences('room', 'docname', [])).rejects.toThrow(error);
+			});
+		});
+	});
+
+	describe('deleteDocument', () => {
+		describe('when listObjectsV2 rejects', () => {
+			const setup = () => {
+				const error = new Error();
+				const returnValue = { toArray: jest.fn().mockRejectedValueOnce(error) };
+				client.listObjectsV2.mockReturnValueOnce(returnValue as unknown as BucketStream<BucketItem>);
+
+				return { error };
+			};
+
+			it('should reject', async () => {
+				const { error } = setup();
+
+				await expect(service.deleteDocument('room', 'docname')).rejects.toThrow(error);
+			});
+		});
+
+		describe('when listObjectsV2 resolves', () => {
+			describe('when removeObjects resolves', () => {
+				const setup = () => {
+					const objNames = [{ name: 'obj1' }, { name: 'obj2' }];
+					const returnValue = { toArray: jest.fn().mockResolvedValueOnce(objNames) };
+					client.listObjectsV2.mockReturnValueOnce(returnValue as unknown as BucketStream<BucketItem>);
+
+					return { objNames };
+				};
+
+				it('should call removeObjects', async () => {
+					setup();
+
+					await service.deleteDocument('room', 'docname');
+
+					expect(client.removeObjects).toHaveBeenCalledWith(bucketName, ['obj1', 'obj2']);
+				});
+			});
+
+			describe('when removeObjects rejects', () => {
+				const setup = () => {
+					const objNames = [{ name: 'obj1' }, { name: 'obj2' }];
+					const returnValue = { toArray: jest.fn().mockResolvedValueOnce(objNames) };
+					client.listObjectsV2.mockReturnValueOnce(returnValue as unknown as BucketStream<BucketItem>);
+
+					const error = new Error();
+					client.removeObjects.mockRejectedValueOnce(error);
+
+					return { error };
+				};
+
+				it('should reject', async () => {
+					const { error } = setup();
+
+					await expect(service.deleteDocument('room', 'docname')).rejects.toThrow(error);
+				});
+			});
+		});
+	});
+
+	describe('destroy', () => {
+		it('should throw an error', () => {
+			expect(() => service.destroy()).toThrowError('Method not implemented.');
+		});
+	});
 });
