0e142b4b09028529cda8ee9b4313e32df0538cf8
Max Bischof <maximilian.bischof@dataport.de>
2024-11-11 10:56:49 +0100

    Add validation pipe to server modul

--- src/modules/server/api/dto/tldraw-document-delete.params.ts
+++ src/modules/server/api/dto/tldraw-document-delete.params.ts
@@ -1,3 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsMongoId } from 'class-validator';
+
 export class TldrawDocumentDeleteParams {
+	@IsMongoId()
+	@ApiProperty()
 	public parentId!: string;
 }
