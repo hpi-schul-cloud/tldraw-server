fe0818be056c845c3a36615ba5c1ba38d47a5ff4
SevenWaysDP <sergej.hoffmann@dataport.de>
2024-11-27 10:50:49 +0100

    Add metrics configuration and initialization for default metrics collection

--- src/infra/metrics/metrics.module.ts
+++ src/infra/metrics/metrics.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigurationModule } from '../configuration/configuration.module.js';
 import { LoggerModule } from '../logger/logger.module.js';
 import { MetricsController } from './api/metrics.controller.js';
+import { MetricConfig } from './metrics.config.js';
 import { MetricsService } from './metrics.service.js';
 
 @Module({
-	imports: [LoggerModule],
+	imports: [LoggerModule, ConfigurationModule.register(MetricConfig)],
 	controllers: [MetricsController],
 	providers: [MetricsService],
 })
