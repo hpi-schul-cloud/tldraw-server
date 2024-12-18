0c13bbcf864faad2e1020b761a61b2ce31cd27aa
Constantin Bergatt <constantin.bergatt@dataport.de>
2024-12-18 15:04:43 +0100

    BC-7171 - Refactor TldrawServerConfig to add websocket configuration and reorder properties

--- src/modules/server/tldraw-server.config.ts
+++ src/modules/server/tldraw-server.config.ts
@@ -2,9 +2,15 @@ import { Transform } from 'class-transformer';
 import { IsArray, IsBoolean, IsNumber, IsString, IsUrl } from 'class-validator';
 
 export class TldrawServerConfig {
-	@Transform(({ value }) => value === 'true')
-	@IsBoolean()
-	public FEATURE_TLDRAW_ENABLED!: boolean;
+	@IsString()
+	public TLDRAW_WEBSOCKET_PATH = '';
+
+	@IsNumber()
+	@Transform(({ value }) => parseInt(value))
+	public TLDRAW_WEBSOCKET_PORT = 3345;
+
+	@IsUrl({ protocols: ['wss', 'ws'], require_tld: false })
+	public TLDRAW_WEBSOCKET_URL!: string;
 
 	@Transform(({ value }) => value === 'true')
 	@IsBoolean()
@@ -18,15 +24,9 @@ export class TldrawServerConfig {
 	@IsArray()
 	public TLDRAW_ASSETS_ALLOWED_MIME_TYPES_LIST = ['image/png', 'image/jpeg', 'image/gif', 'image/svg+xml'];
 
-	@IsString()
-	public TLDRAW_WEBSOCKET_PATH = '';
-
-	@IsNumber()
-	@Transform(({ value }) => parseInt(value))
-	public TLDRAW_WEBSOCKET_PORT = 3345;
-
-	@IsUrl({ protocols: ['wss', 'ws'], require_tld: false })
-	public TLDRAW_WEBSOCKET_URL!: string;
+	@Transform(({ value }) => value === 'true')
+	@IsBoolean()
+	public FEATURE_TLDRAW_ENABLED!: boolean;
 
 	@IsString()
 	public NOT_AUTHENTICATED_REDIRECT_URL = '/login';
