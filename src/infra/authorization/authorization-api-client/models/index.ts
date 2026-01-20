dd83e9db0c803d4a7930b7bbd974e5fe5472ca5b
Christian Darsow <hello@darsow.io>
2026-01-20 11:24:23 +0100

    Fix filename

--- src/infra/authorization/authorization-api-client/models/index.ts
+++ src/infra/authorization/authorization-api-client/models/index.ts
@@ -3,4 +3,4 @@
 export * from './api-validation-error.js';
 export * from './authorization-body-params.js';
 export * from './authorization-context-params.js';
-export * from './authorized-reponse.js';
+export * from './authorized-response.js';
